4e48bb3fecd972506c799ff3bf2d28f8e0a5f96a
adampagels <pagels@hotmail.ca>
2020-06-21 12:34:56 -0400

    Add Navigation to Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import Navigation from "../components/navigation"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,20 +24,15 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          margin: `auto`,
+          maxWidth: "1200px",
+          padding: `1rem 1.0875rem 1.45rem`,
         }}
       >
+        <Navigation />
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     </>
   )
