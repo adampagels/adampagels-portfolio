45c0704d2e31069728062a06d5ff428d262bf667
adampagels <pagels@hotmail.ca>
2020-06-21 12:39:14 -0400

    Update background styling

--- src/components/Background.js
+++ src/components/Background.js
@@ -8,14 +8,17 @@ const Background = () => {
         style={{
           backgroundImage: `url(${backgroundImage})`,
           backgroundRepeat: "no-repeat",
+          backgroundSize: '100%',
           width: "100%",
           maxWidth: "1200px",
-          height: "60%",
+          height: "50%",
           position: "fixed",
           bottom: "0px",
           right: "0px",
           left: "0px",
           margin: "0 auto",
+          border: '1px solid',
+          borderBottom: '0px'
         }}
       ></div>
     </div>
