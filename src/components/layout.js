1297bca3f720a4c68c5461b582e919d912b9b5ac
adampagels <pagels@hotmail.ca>
2020-06-21 12:48:15 -0400

    Update imports

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Navigation from "../components/navigation"
-import "./layout.css"
+import Navigation from "../components/Navigation"
+import "./Layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
