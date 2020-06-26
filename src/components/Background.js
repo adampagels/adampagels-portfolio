553cb4d0af22abc44870f0eee582deeeae29423d
adampagels <pagels@hotmail.ca>
2020-06-26 00:11:55 -0400

    Add media queries

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,26 +1,35 @@
 import React from "react"
 import backgroundImage from "../images/backgroundImage.svg"
+import styled from "styled-components"
+
+const StyledBackground = styled.div`
+  background-image: url(${backgroundImage});
+  background-repeat: no-repeat;
+  background-size: 150%;
+  width: 95%;
+  max-width: 1200px;
+  height: 50%;
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  left: 0px;
+  margin: 0 auto;
+  border: 1px solid;
+  border-bottom: 0px;
+
+  @media (min-width: 700px) {
+    background-size: 100%;
+  }
+
+  @media (min-width: 1200px) {
+    width: 100%;
+  }
+`
 
 const Background = () => {
   return (
     <div>
-      <div
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: '100%',
-          width: "100%",
-          maxWidth: "1200px",
-          height: "50%",
-          position: "fixed",
-          bottom: "0px",
-          right: "0px",
-          left: "0px",
-          margin: "0 auto",
-          border: '1px solid',
-          borderBottom: '0px'
-        }}
-      ></div>
+      <StyledBackground />
     </div>
   )
 }
