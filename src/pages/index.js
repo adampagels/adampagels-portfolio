f2a45286dbb200893a136f7d38b690f8094f5e26
adampagels <pagels@hotmail.ca>
2020-06-25 22:12:02 -0400

    Add media queries for H1 font-size

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,42 +3,39 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundImage from "../components/Background"
+import styled from "styled-components"
+
+const StyledH1 = styled.h1`
+  color: #111;
+  font-size: 50px;
+  text-align: center;
+
+  @media (min-width: 400px) {
+    font-size: 60px;
+  }
+
+  @media (min-width: 485px) {
+    font-size: 80px;
+  }
+`
+
+const StyledH2Container = styled.div`
+  margin-top: -40px;
+`
+
+const StyledH2 = styled.h2`
+color: #111,
+fontSize: 25,
+`
 
 const Index = () => (
   <Layout>
     <SEO title="Home" />
-    <h1
-      style={{
-        color: "black",
-        fontSize: 80,
-        textAlign: "center",
-        paddingRight: 0,
-      }}
-    >
-      Adam Pagels
-    </h1>
-    <div style={{ marginTop: "-40px" }}>
-      <h2
-        style={{
-          color: "black",
-          fontSize: 25,
-          marginLeft: "50%",
-          paddingRight: 0,
-        }}
-      >
-        Full-Stack Developer
-      </h2>
-      <h2
-        style={{
-          color: "black",
-          fontSize: 25,
-          marginLeft: "50%",
-          paddingRight: 0,
-        }}
-      >
-        Based in Toronto
-      </h2>
-    </div>
+    <StyledH1>Adam Pagels</StyledH1>
+    <StyledH2Container>
+      <StyledH2>Full-Stack Developer</StyledH2>
+      <StyledH2>Based in Toronto</StyledH2>
+    </StyledH2Container>
     <BackgroundImage />
   </Layout>
 )
