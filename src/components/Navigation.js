d8ac6303d3e900d8a1cce2f91cc1f233eaa89172
adampagels <pagels@hotmail.ca>
2020-07-06 23:43:14 -0400

    Update styling for device widths under 830px

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,24 +4,23 @@ import styled from "styled-components"
 
 const StyledNav = styled.nav`
   display: flex;
-  font-size: 18px;
   list-style: none;
   width: 100%;
-  height: 10vh;
   display: flex;
-  background-color: transparent;
+  background-color: #fbfbfb;
   position: relative;
   justify-content: space-between;
   text-transform: lowercase;
-  margin: 0 auto;
+  margin: -20px auto 0px auto;
   align-self: center;
-  margin-top: 20px;
   z-index: 6;
 
   @media (max-width: 830px) {
+    transition: all 0.22s ease-out;
+    background-color: ${props => (!props.open ? "transparent" : "#fbfbfb")};
     margin-top: 0px;
-    position: sticky;
-    height: 8vh;
+    position: fixed;
+    height: 70px;
     top: 0;
     left: 0;
     right: 0;
@@ -36,20 +35,66 @@ const StyledLink = styled(Link)`
   text-decoration: none;
   padding: 10px;
   color: #7158d1;
-
   &:visited {
     color: #7158d1;
+  }
+
+  @media (max-width: 830px) {
+    color: #fbfbfb;
+
+    &:visited {
+      color: #fbfbfb;
+    }
+  }
+`
+
+const StyledHeaderLink = styled(Link)`
+  display: none;
+
+  @media (max-width: 830px) {
+    padding-left: 0px;
+    font-size: 18px;
+    text-decoration: none;
+    display: block;
+    position: fixed;
+    top: 20px;
+    left: 25px;
+    color: #7158d1;
+
+    &:visited {
+      color: #7158d1;
+    }
   }
 `
 
 const StyledOuterUl = styled.ul`
   display: flex;
   padding: 0px;
+
+  @media (max-width: 830px) {
+    margin: 0px auto;
+    width: 100%;
+    justify-content: center;
+
+    &:first-child {
+      display: none;
+    }
+  }
 `
 
 const StyledInnerUl = styled.ul`
   margin: 0px auto;
   display: flex;
+  padding-left: 0px;
+
+  @media (max-width: 830px) {
+    flex-direction: column;
+    padding-left: 0px;
+    align-items: center;
+    width: 100%;
+    justify-content: center;
+    margin-top: 70px;
+  }
 `
 
 const StyledInnerLi = styled.li`
@@ -58,17 +103,32 @@ const StyledInnerLi = styled.li`
   padding: 10px;
   margin-right: 20px;
   font-family: sans-serif;
+
+  @media (max-width: 830px) {
+    border: 1px solid #8c6eff;
+    justify-content: center;
+    margin-right: 0px;
+    width: 100%;
+    background-color: #7158d1;
+  }
 `
 
 const StyledOuterLi = styled.li`
   display: flex;
   text-decoration: none;
+  padding: 10px;
   font-family: sans-serif;
+
+  @media (max-width: 830px) {
+    border: 1px solid #8c6eff;
+    width: 100%;
+    justify-content: center;
+    background-color: #7158d1;
+  }
 `
 
 const Navbox = styled.div`
   display: flex;
-  height: 40%;
   justify-content: flex-end;
   align-items: center;
   width: 100%;
@@ -78,53 +138,45 @@ const Navbox = styled.div`
     position: fixed;
     width: 100%;
     justify-content: flex-start;
-    padding-top: 10vh;
-    background-color: red;
-    transition: all 0.3s ease-in;
-    top: 8vh;
-    top: ${props => (props.open ? "-100%" : "0")};
+    transition: all 0.4s ease-in-out;
+    top: ${props => (props.open ? "-180%" : "0")};
     left: 0px;
   }
 `
 
 const Hamburger = styled.div`
   display: none;
-  background-color: #111;
+  background-color: #7158d1;
   width: 30px;
   height: 3px;
   transition: all 0.3s linear;
   align-self: center;
-  position: absolute;
-  right: 14px;
+  position: fixed;
+  right: 18px;
   top: 30px;
   transform: ${props => (props.open ? "rotate(-45deg)" : "inherit")};
-
   ::before,
   ::after {
     width: 30px;
     height: 3px;
-    background-color: #111;
+    background-color: #7158d1;
     content: "";
     position: absolute;
     transition: all 0.3s linear;
   }
-
   ::before {
     transform: ${props =>
       props.open ? "rotate(-90deg) translate(-10px, 0px)" : "rotate(0deg)"};
     top: -10px;
   }
-
   ::after {
     opacity: ${props => (props.open ? "0" : "1")};
     transform: ${props => (props.open ? "rotate(90deg) " : "rotate(0deg)")};
     top: 10px;
   }
-
   :hover {
     cursor: pointer;
   }
-
   @media (max-width: 830px) {
     display: flex;
 `
@@ -158,8 +210,9 @@ const Navigation = () => {
     </>
   )
   return (
-    <StyledNav>
+    <StyledNav open={navbarOpen}>
       {navbarOpen ? <Navbox>{navbar}</Navbox> : <Navbox open>{navbar}</Navbox>}
+      <StyledHeaderLink to="/">Adam Pagels</StyledHeaderLink>
       {navbarOpen ? (
         <Hamburger open={"open"} onClick={() => setNavbarOpen(!navbarOpen)} />
       ) : (
