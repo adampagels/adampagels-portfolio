c7ef54e93e4f007081750a27201b1f93f470db52
adampagels <pagels@hotmail.ca>
2020-07-23 22:53:53 -0400

    Add header

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,25 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import styled from "styled-components"
 import SEO from "../components/seo"
+
+const StyledH1 = styled.h1`
+  color: #333;
+  font-family: sans-serif;
+  font-size: 60px;
+  text-align: center;
+  margin-top: 70px;
+
+  @media screen and (min-width: 930px) {
+    margin-top: 40px;
+  }
+`
 
 const Blog = () => (
   <Layout>
     <SEO title="Blog" />
-    <h1>Blog</h1>
+    <StyledH1>blog coming soon</StyledH1>
   </Layout>
 )
 
