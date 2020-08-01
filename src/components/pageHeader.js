e67431bb4fda010eeed64d286edef06400131e5d
adampagels <pagels@hotmail.ca>
2020-08-01 16:00:11 -0400

    Reduce font-size on devices with widths smaller than 475px

--- src/components/pageHeader.js
+++ src/components/pageHeader.js
@@ -3,9 +3,13 @@ import styled from "styled-components"
 
 const StyledH1 = styled.h1`
   color: #333;
-  font-size: 60px;
+  font-size: 50px;
   text-align: center;
   margin-top: 70px;
+
+  @media screen and (min-width: 475px) {
+    font-size: 60px;
+  }
 
   @media screen and (min-width: 830px) {
     margin-top: 40px;
