6c8fc4a96251fba693938f59cbb27674cf01e08e
adampagels <pagels@hotmail.ca>
2020-08-01 08:57:38 -0400

    Add portrait image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,24 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/image"
+import styled from "styled-components"
+
+const StyledContentContainer = styled.div``
+
+const StyledImageWrapper = styled.div``
+
+const StyledImage = styled(Image)``
 
 const About = () => (
   <Layout>
     <SEO title="About" />
     <h1>About</h1>
+    <StyledContentContainer>
+      <StyledImageWrapper>
+        <StyledImage />
+      </StyledImageWrapper>
+    </StyledContentContainer>
   </Layout>
 )
 
