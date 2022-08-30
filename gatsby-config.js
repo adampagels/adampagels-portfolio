a3bd2253010b6b5e32abb045ba8ffee50ba8738c
Adam Pagels <pagels@hotmail.ca>
2022-08-30 21:56:49 +0200

    Remove purple background colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,8 +51,6 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/adam-pagels-icon.png`,
       },
