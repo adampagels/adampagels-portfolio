109d8582dd50aa9bb3d22da1836d6f5177a3928b
Adam Pagels <pagels@hotmail.ca>
2022-09-06 21:17:22 +0200

    Work on blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/src/blog`,
         name: "blog-posts",
       },
     },
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,24 +5,34 @@ import styled from "styled-components"
 import SEO from "../components/seo"
 import PageHeader from "../components/pageHeader"
 
-const Blog = ({data}) => (
-  <Layout>
-      {console.log(data)}
-    <SEO title="Blog" />
-    <PageHeader heading={"blog coming soon"} />
-  </Layout>
-)
+const Blog = ({ data }) => {
+  const blogPosts = data.allMarkdownRemark.edges
+  console.log(blogPosts)
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <PageHeader heading={"blog coming soon"} />
+    </Layout>
+  )
+}
 
 export default Blog
 
-export const query = graphql`
-    query doing($id: String!) {
-      markdownRemark(id: { eq: $id }) {
-        html
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
+export const blogPageQuery = graphql`
+  query BlogPage {
+    allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            author
+            date
+            tags
+          }
         }
       }
     }
-  `
+  }
+`
